ba0f403aa994aaaf6573f959f918d922afcaba99
GinaDeBeukelaer <gientje86>
2016-04-11 13:50:26 +0200

    add entity, repository and integration test

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert into Milestone(id, username, createdate, duedate, enddate, moreinformation) values (1, 'gide', '2016-02-01', '2016-12-31', '2016-03-01', 'time to upgrade from java 6 to java 8 certificate.');
