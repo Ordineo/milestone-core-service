b25050b1c2d8a333a747ade5bbe9f369b7b48864
Philippe Damen <mr.deleeuw@gmail.com>
2016-04-12 10:10:54 +0200

    restdocs fixes

--- src/test/java/be/ordina/ordineo/ObjectiveIntegrationTest.java
+++ src/test/java/be/ordina/ordineo/ObjectiveIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.restdocs.RestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
