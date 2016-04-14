3ebe8cdec610007c81a1da555e5303f2e8203529
Philippe Damen <mr.deleeuw@gmail.com>
2016-04-14 11:38:42 +0200

    added adjusted testdata

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,10 +5,12 @@ INSERT INTO objective_tags(objective_id, tag) VALUES (1, 'spring');
 INSERT INTO objective(id, title, description, objectiveType) VALUES (2, 'Java Certificate', 'description certificate', 'CERTIFICATE');
 INSERT INTO objective_tags(objective_id, tag) VALUES (2, 'java');
 
+INSERT INTO objective(id, title, description, objectiveType) VALUES (3, 'Scrum Master', 'description certificate', 'CERTIFICATE');
+INSERT INTO objective_tags(objective_id, tag) VALUES (3, 'scrum');
+
 INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (1, 2, 'gide', '2016-02-01', '2016-12-31', '2016-03-01', 'time to upgrade from java 6 to java 8 certificate.');
 INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (2, 1, 'gide', '2017-02-01', '2017-12-31', null, 'extra spring milestone');
 
-INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (3, 2, 'Rydg', '2016-02-01', '2016-12-31', '2016-03-01', 'time to upgrade from java 6 to java 8 certificate.');
-INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (4, 1, 'Rydg', '2017-02-01', '2017-12-31', null, 'extra spring milestone');
-INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (5, 2, 'Rydg', '2016-02-01', '2016-12-31', '2016-03-01', 'time to upgrade from java 6 to java 8 certificate.');
-INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (6, 1, 'Rydg', '2017-02-01', '2017-12-31', null, 'extra spring milestone');
+INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (3, 2, 'Rydg', '2016-02-05', '2016-12-31', '2016-03-01', 'Getting my first java certificate');
+INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (4, 1, 'Rydg', '2017-06-01', '2017-12-31', null, 'extra spring milestone');
+INSERT INTO milestone(id, objective_id, username, createdate, duedate, enddate, moreinformation) VALUES (5, 3, 'Rydg', '2016-02-05', '2016-12-31', '2016-03-01', 'Getting a scrum master certificate');
