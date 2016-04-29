fc5354f339eea0a356e4639208b489bcb46e473a
Philippe Damen <mr.deleeuw@gmail.com>
2016-04-29 10:14:42 +0200

    added filter to check for jwt token

--- src/main/java/be/ordina/ordineo/MilestoneCoreApplication.java
+++ src/main/java/be/ordina/ordineo/MilestoneCoreApplication.java
@@ -1,9 +1,12 @@
 package be.ordina.ordineo;
 
+import be.ordina.ordineo.security.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.retry.annotation.EnableRetry;
 
@@ -23,4 +26,12 @@ public class MilestoneCoreApplication {
         SpringApplication.run(MilestoneCoreApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
+
+        return registrationBean;
+    }
 }
